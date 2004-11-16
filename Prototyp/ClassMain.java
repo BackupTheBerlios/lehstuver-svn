73a36f835100d41207b1af63ca40b57da0d4921c
mockfrog <mockfrog@a9bfd9a8-47dc-0310-a891-c459b15d62a1>
2004-11-16 09:02:51 +0000

    test

--- Prototyp/ClassMain.java
+++ Prototyp/ClassMain.java
@@ -8,8 +8,7 @@
 /**
  * @author markusschlichting
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
  */
 public class ClassMain {
 
